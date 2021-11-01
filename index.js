e1a3b79be2cb0b378d561f5bc0cc355226c54f77
Ona <onushkis@gmail.com>
2021-11-01 14:18:04 +0100

    add js btnt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let jokeItem = document.querySelector(".jokeItem");
 let touchCordinateStart;
 let touchCordinateMove;
+let deleteButtonContainer = (window.screen.width * 40) / 100;
 
 jokeItem.addEventListener("touchstart", (e) => {
  touchCordinateStart = e.touches[0].clientX;
@@ -10,8 +11,11 @@ jokeItem.addEventListener("touchmove", (e) => {
     touchCordinateMove = Math.floor(e.touches[0].clientX);
   
   
-    if(touchCordinateMove < touchCordinateStart){
-    jokeItem.style.transform = `translateX(${touchCordinateMove - touchCordinateStart}px)`;
+    if(touchCordinateMove < touchCordinateStart  && 
+    touchCordinateMove > touchCordinateStart - deleteButtonContainer
+    ){
+    jokeItem.style.transform = `translateX(${
+    touchCordinateMove - touchCordinateStart}px)`;
 
   }
 
