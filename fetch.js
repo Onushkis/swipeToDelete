2a47c79048dac17e61798ca7bda2b4f1306fb010
Ona <onushkis@gmail.com>
2021-11-02 13:28:16 +0100

    add innerHtml

--- fetch.js
+++ fetch.js
@@ -1,13 +1,22 @@
-
 axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
   const users = response.data;
-  const jokeWrapper = document.querySelector("jokeWrapper");
-    const section = document.createElement("section", {className: 'animate__animated'});
-  const deleteItem = document.createElement("div", {className: 'deleteItem'});
-  const jokeItem = document.createElement("artickle", {className: 'jokeItem'});
-//   console.log(`GET users`, users);
-//   const joke = document.createTextNode(users[0].name);
-  jokeItem. textContent = users[0].name;
+
+  users.forEach((user) => {
+    const jokeWrapper = document.querySelector(".jokeWrapper");
+
+    const section = document.createElement("section");
+    section.classList.add("animate__animated", "DeletAnimation");
+
+    const deleteItem = document.createElement("div");
+    deleteItem.classList.add("deleteItem");
+
+    const jokeItem = document.createElement("article");
+    jokeItem.classList.add("jokeItem");
+
+    section.appendChild(deleteItem);
+    section.appendChild(jokeItem);
+    jokeWrapper.appendChild(section);
+
+    jokeItem.textContent = user.name;
+  });
 });
-
-
