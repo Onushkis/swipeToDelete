bbe166a094dff0ff2c8008fe69831b4dbfd1f0a4
Ona <onushkis@gmail.com>
2021-11-02 11:30:14 +0100

    add fetch

--- fetch.js
+++ fetch.js
@@ -1,7 +1,13 @@
+
 axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
   const users = response.data;
-  console.log(`GET users`, users);
+  const jokeWrapper = document.querySelector("jokeWrapper");
+    const section = document.createElement("section", {className: 'animate__animated'});
+  const deleteItem = document.createElement("div", {className: 'deleteItem'});
+  const jokeItem = document.createElement("artickle", {className: 'jokeItem'});
+//   console.log(`GET users`, users);
+//   const joke = document.createTextNode(users[0].name);
+  jokeItem. textContent = users[0].name;
 });
-
 
 
